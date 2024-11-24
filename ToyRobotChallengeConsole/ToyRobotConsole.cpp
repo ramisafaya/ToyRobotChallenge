fcecde2105098a628de99edcdbd29536f87ac1c3
Rami Safaya <rami.safaya@gmail.com>
2024-11-25 09:55:05 +1100

    removed win check since not needed

--- ToyRobotChallengeConsole/ToyRobotConsole.cpp
+++ ToyRobotChallengeConsole/ToyRobotConsole.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 
-#if defined(_WIN32) || defined(_WIN64)
-	#include "Windows.h"
-#endif
+#include "Windows.h"
 
 #include "../ToyRobot/Robot.h"
 #include "../ToyRobot/TableTop.h"
@@ -12,7 +10,6 @@
 static const int TABLE_WIDTH = 5;
 static const int TABLE_HEIGHT = 5;
 
-#if defined(_WIN32) || defined(_WIN64)
 BOOL WINAPI CtrlHandler(DWORD fdwCtrlType)
 {
 	if (fdwCtrlType == CTRL_C_EVENT)
@@ -22,17 +19,14 @@ BOOL WINAPI CtrlHandler(DWORD fdwCtrlType)
 
 	return FALSE;
 }
-#endif
 
 int main()
 {
-#if defined(_WIN32) || defined(_WIN64)
 	if (!SetConsoleCtrlHandler(CtrlHandler, TRUE))
 	{
 		std::cout << "ERROR: unable to register Control C handler, exiting." << std::endl;
 		return 1;
 	}
-#endif
 
 	std::shared_ptr<Robot> robot(new Robot());
 	std::shared_ptr<TableTop> table(new TableTop(TABLE_WIDTH, TABLE_HEIGHT));
